628884edf59291c28d26d20da1c410142d2dd117
Naufal Rasyid <naufalrasyid86@gmail.com>
2017-11-09 09:00:37 +0700

    Add createFromDate -> age Mengetahui umur

--- 06_composer/index.php
+++ 06_composer/index.php
@@ -3,4 +3,6 @@ require 'vendor/autoload.php';
 
 use Carbon\Carbon;
 
-echo "Sekarang : ". Carbon::now();
+echo "Sekarang : ". Carbon::now()."<br>";
+
+echo "Umur Saya : ". Carbon::createFromDate(1997, 10, 24)->age."<br>';
